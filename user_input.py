ece31f8c47de5762ace29f48de59681103dfb41a
JayneNjeri <jaynenjeri739@gmail.com>
2024-02-23 19:44:03 +0300

    Python Assignment that enables user input,i.e, name, age and location

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#User Input
+# The input() function allows for user input. The input function takes a single argument, which is the prompt message. The prompt message is displayed to the user, and the program waits for the user to enter a value. The value entered by the user is returned by the input function.
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your location: ")
+
+# Print a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
